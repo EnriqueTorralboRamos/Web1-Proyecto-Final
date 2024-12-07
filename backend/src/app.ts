92ca0038b8e8a8c2e3a2e5ba70b5e0b0af2c37c5
EnriqueTorralboRamos <102521084+EnriqueTorralboRamos@users.noreply.github.com>
2024-12-07 21:06:02 +0100

    fix cors problem

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,8 +3,23 @@ import userRoutes from './routes/userRoutes';
 import authRoutes from './routes/authRoutes';
 import countryRoutes from './routes/countryRoutes';
 import programRoutes from './routes/programRoutes';
+import cors from 'cors';
 
 const app = express();
+console.log('Frontend URL:', process.env.FRONTEND_URL);
+// Configurar CORS para permitir solicitudes desde el frontend
+app.use(cors({
+  origin: (origin, callback) => {
+    console.log('Origen recibido en CORS:', origin); // Depuración
+    if (!origin || origin === "http://localhost:3000") {
+      callback(null, true); // Permitir el origen
+    } else {
+      callback(new Error('No permitido por CORS')); // Bloquear otros orígenes
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
+  credentials: true, // Permitir cookies o credenciales
+}));
 //para debuggear
 app.use((req, res, next) => {
     console.log(`Solicitud recibida en app.ts: ${req.method} ${req.url}`);
